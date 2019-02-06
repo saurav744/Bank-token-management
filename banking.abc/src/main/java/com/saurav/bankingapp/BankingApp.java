bba8abb5184ccaec6cef98f9589f5e28e075a65e
Saurav Sharma <saurav.sharma@imaginea.com>
2019-02-06 12:58:00 +0530

    abc banking assignment

--- banking.abc/src/main/java/com/saurav/bankingapp/BankingApp.java
+++ banking.abc/src/main/java/com/saurav/bankingapp/BankingApp.java
@@ -1,5 +1,13 @@
 package com.saurav.bankingapp;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BankingApp {
+	
+	public static void main(String args[]) {
+		SpringApplication.run(BankingApp.class);
+	}
 
 }
